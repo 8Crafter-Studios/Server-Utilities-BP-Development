e7bf8db32c90c597311e82ebed00ccc35c74feac
Andexter8 <8crafteryt@gmail.com>
2024-11-24 21:19:10 -0800

    Bug Fix

--- src/init/prototypeExtensions/String.ts
+++ src/init/prototypeExtensions/String.ts
@@ -375,7 +375,8 @@ Object.defineProperties(String.prototype, {
         writable: true,
     },
 });
-export const exports_5603749806156139082470132985463298047098135609812364098 = void undefined as void;
+export const exports_5603749806156139082470132985463298047098135609812364098 =
+    undefined;
 declare global {
     interface String {
         escapeCharacters(
