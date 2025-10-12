04a89e90fb9b250aef24216eb43e443c49f926e7
8Crafter (PC) <8crafteryt@gmail.com>
2025-10-12 11:50:07 -0700

    v1.40.1+BUILD.1

--- scripts/initializeMainGlobalVariables.d.ts
+++ scripts/initializeMainGlobalVariables.d.ts
@@ -6,7 +6,7 @@ export declare namespace mainGlobalVariables {
     /**
      * The version of the add-on.
      */
-    const current_format_version = "1.40.0+BUILD.1";
+    const current_format_version = "1.40.1+BUILD.1";
     /**
      * The version of Minecraft Bedrock Edition supported by the add-on.
      */
--- scripts/initializeMainGlobalVariables.js
+++ scripts/initializeMainGlobalVariables.js
@@ -13,7 +13,7 @@ export var mainGlobalVariables;
     /**
      * The version of the add-on.
      */
-    mainGlobalVariables.current_format_version = "1.40.0+BUILD.1";
+    mainGlobalVariables.current_format_version = "1.40.1+BUILD.1";
     /**
      * The version of Minecraft Bedrock Edition supported by the add-on.
      */
